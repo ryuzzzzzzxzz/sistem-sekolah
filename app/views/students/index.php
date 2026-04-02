a8cea98880fbc21232c70b08b66976c805474c36
ryuzzzzzzxzz <ryufelix45@gmail.com>
2026-04-02 13:26:20 +0700

    feat: implement student index page

--- app/views/students/index.php
+++ app/views/students/index.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Daftar Siswa</title>
     <link rel="stylesheet" href="/css/output.css">
 </head>
-<body class="min-h-screen flex flex-col">
+
+<body class="min-h-screen flex flex-col bg-gray-100">
     <!-- header start -->
     <header class="bg-blue-500 text-white ">
         <div class="flex items-center justify-between container mx-auto p-4">
@@ -16,23 +18,55 @@
     </header>
     <!-- header end -->
     <!-- main start -->
-     <main class="container mx-auto grow">
-        <div class="mt-8">
+    <main class="container mx-auto grow">
+        <div class="mt-8 space-y-2">
             <!-- card header start -->
-             <div class="p-4 shadow rounded-lg ">
+            <div class="p-4 shadow rounded-lg bg-white">
                 <h1 class="text-2xl font-bold">Daftar Siswa</h1>
                 <p>Menampilkan Daftar Siswa Yang Terdaftar</p>
-             </div>
+            </div>
             <!-- card header end -->
-        </div>
-     </main>
+
+            <!-- Card  Body Start -->
+            <div class="bg-white shadow rounded-lg">
+                <table class="w-full">
+                    <thead class="bg-gray-200">
+                        <tr>
+                            <th class="px-4 py-2 text-left">No</th>
+                            <th class="px-4 py-2 text-left">Nama</th>
+                            <th class="px-4 py-2 text-left">NIS</th>
+                            <th class="px-4 py-2 text-left">Kelas</th>
+                            <th class="px-4 py-2 text-left">No Telepon</th>
+                            <th class="px-4 py-2">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="px-4 py-2 text-left">1</td>
+                            <td class="px-4 py-2 text-left">Andi</td>
+                            <td class="px-4 py-2 text-left">1234</td>
+                            <td class="px-4 py-2 text-left">11 tkj 2</td>
+                            <td class="px-4 py-2 text-left"> 0811111203</td>
+                            <td class="px-4 py-2">
+                                <div class="flex justify-center items-center gap-4">
+                                    <a href="" class="text-green-500">Detail</a>
+                                    <a href="" class="text-yellow-500">Edit</a>
+                                    <a href="" class="text-red-500">Hapus</a>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+    </main>
     <!-- main end -->
     <!-- footer start -->
-     <footer class="bg-gray-900 text-white">
+    <footer class="bg-gray-900 text-white">
         <div class="text-center p-4">
-            &copy; <?= date(format:'Y') ?> Sistem Sekolah - SMK Kristen Immanuel. All rights reserved.
+            &copy; <?= date(format: 'Y') ?> Sistem Sekolah - SMK Kristen Immanuel. All rights reserved.
         </div>
-     </footer>
+    </footer>
     <!-- footer end -->
 </body>
+
 </html>
